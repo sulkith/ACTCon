c27ffe2d50e0963729f4f74792da01614d0c74d7
Michael Drost <sulkith@gmail.com>
2016-01-16 14:05:58 +0100

    Added a WS2812 LED Strip for the RPM. Softwar is also compatible with a Sparkfun Pro Micro now. The Sparkfun chip is only working with the new Host Application from the last commit.

--- Teensy/ACTCon/ACTCon.ino
+++ Teensy/ACTCon/ACTCon.ino
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Michael Drost
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,10 +22,21 @@
  * SOFTWARE.
  */
 
+#include <FastLED.h>
+
+#define LED_PIN     3
+
+// Information about the LED strip itself
+#define NUM_LEDS    34
+#define CHIPSET     WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+#define BRIGHTNESS  32
+
 int percentage;
 int rpm;
 int rpmt;
-int maxrpm;
+long maxrpm;
 enum state_t
 {
   idle,
@@ -34,7 +45,7 @@ enum state_t
   recrpm,
   finrpm,
   fin
-}state;
+} state;
 const int ledPin = 13;
 const int FFPin = 12;
 void setup()
@@ -46,89 +57,109 @@ void setup()
   digitalWrite(FFPin, HIGH);
   percentage = 0;
   rpm = 0;
-  rpmt=0;
+  rpmt = 0;
   maxrpm = 1000;
-  state=idle;
+  state = idle;
+
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalSMD5050 );
+  FastLED.setBrightness( BRIGHTNESS );
 }
 
 
 void loop()
 {
-  
-//check serial and receive messages
-  if(Serial.available())
+
+  //check serial and receive messages
+  if (Serial.available())
   {
-//receive
+    //receive
     byte incomingByte = Serial.read();
-//decode vibration level
-    if(state == rec && incomingByte >= '0' && incomingByte <= '9')
+    //decode vibration level
+    if (state == rec && incomingByte >= '0' && incomingByte <= '9')
     {
       percentage *= 10;
-      percentage += incomingByte -'0';
+      percentage += incomingByte - '0';
       Serial.print("Percentage Set to: ");
       Serial.println(percentage);
     }
-//decode rpm
-    if(state == recrpm && incomingByte >= '0' && incomingByte <= '9')
+    //decode rpm
+    if (state == recrpm && incomingByte >= '0' && incomingByte <= '9')
     {
       rpmt *= 10;
-      rpmt += incomingByte -'0';
+      rpmt += incomingByte - '0';
       Serial.print("rpm Set to: ");
       Serial.println(rpmt);
     }
-//find start and stop Bytes A R and stop E r
-    if(incomingByte == 'A')
+    //find start and stop Bytes A R and stop E r
+    if (incomingByte == 'A')
     {
       state = rec;
     }
-    if(incomingByte == 'R')
+    if (incomingByte == 'R')
     {
       rpmt = 0;
       state = recrpm;
     }
-    if(incomingByte == 'E' && state == rec)
+    if (incomingByte == 'E' && state == rec)
     {
-      state = fin;  
+      state = fin;
     }
-    if(incomingByte == 'r' && state == recrpm)
+    if (incomingByte == 'r' && state == recrpm)
     {
-      state = idle;  
+      state = idle;
       rpm = rpmt;
     }
   }
-//apply received values
-  if(state == fin)
+  //apply received values
+  if (state == fin)
   {
     state = idle;
-    if(percentage >100){percentage = 0; return;}
-    if(percentage > 50)
+    if (percentage > 100) {
+      percentage = 0;
+      return;
+    }
+    if (percentage > 50)
     {
-      digitalWrite(FFPin, LOW);  
+      digitalWrite(FFPin, LOW);
     }
     else
     {
-      digitalWrite(FFPin, HIGH);  
+      digitalWrite(FFPin, HIGH);
     }
     percentage = 0;
   }
-  
-//find max rpm
-  if(rpm > maxrpm) maxrpm = rpm;
-//TODO: Filter downshifts --> driving fullspeed in the 6th gear and dunshifting rapidly brings engine to 15000 rpm which is definately not maxrpm
-  
-//LED Controll for max rpm
-  if((rpm > (maxrpm*95/100)))
+
+  //find max rpm
+  if (rpm > maxrpm) maxrpm = rpm;
+  //TODO: Filter downshifts --> driving fullspeed in the 6th gear and dunshifting rapidly brings engine to 15000 rpm which is definately not maxrpm
+
+  //LED Controll for max rpm
+  if ((rpm > (maxrpm * 95 / 100)))
   {
-    if(((millis()/50) % 2)==0)
+    if (((millis() / 50) % 2) == 0)
       digitalWrite(ledPin, HIGH);
     else
       digitalWrite(ledPin, LOW);
   }
-  else if((rpm > (maxrpm*85/100)))
+  else if ((rpm > (maxrpm * 85 / 100)))
   {
     digitalWrite(ledPin, HIGH);
   }
   else
     digitalWrite(ledPin, LOW);
+
+
+  for (int i = 0; i < NUM_LEDS; ++i)
+  {
+    CRGB Color = CRGB::Green;
+    if (i >= 24) Color = CRGB::Yellow;
+    if (i >= 28) Color = CRGB::Red;
+    if ((((long)rpm) * NUM_LEDS / maxrpm) > i)
+      leds[i] = Color;
+    else
+      leds[i] = CRGB::Black;
+  }
+  FastLED.show();
 }
+
 
