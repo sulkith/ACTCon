b63751cfe39328ad5623ad62de84f70ace496dd2
Michael Drost <sulkith@gmail.com>
2016-02-07 11:53:44 +0100

    Filter for maxrpm. Maxrpm is now only measured in N

--- Teensy/ACTCon/rpm.h
+++ Teensy/ACTCon/rpm.h
@@ -6,8 +6,30 @@ void rpm_ini()
 }
 void rpm_cyclic()
 {
+  static int maxrpm_temp = maxrpm;
+  static int maxrpm_deb = 0;
+  
   //find max rpm
-  if (rpm > maxrpm) maxrpm = rpm;
+  if (rpm > maxrpm_temp)
+  {
+      maxrpm_temp = rpm;
+      maxrpm_deb = 50;    
+  }
+  
+  if(gear[0] == 'N')
+  {
+    maxrpm_deb--;  
+  }
+  else
+  {
+    maxrpm_temp = maxrpm;
+    maxrpm_deb = 0;
+  }
+  
+  if(maxrpm_deb == 1)
+  {
+      maxrpm = maxrpm_temp;
+  }
   //TODO: Filter downshifts --> driving fullspeed in the 6th gear and dunshifting rapidly brings engine to 15000 rpm which is definately not maxrpm
 
   rpmprc10 = (rpm * 1000UL)/maxrpm;
