a21d4fd10f17bc0b9be51059cd5dd121b7439f28
Michael Drost <sulkith@gmail.com>
2016-02-22 18:56:38 +0100

    restructured KeyboardOutput to be recognized by AssettoCorsa

--- Teensy/ACTCon/keyboardOutput.h
+++ Teensy/ACTCon/keyboardOutput.h
@@ -21,9 +21,53 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+uint8_t keyboardOutput_key1;
+uint8_t keyboardOutput_key2;
+uint8_t keyboardOutput_key3;
+enum keyboardOutputState_t
+{
+  idle,
+  key2_state,
+  key3_state,
+  releaseKeys
+} keyboardOutputState;
 void keyboardOutput_ini()
 {
   Keyboard.begin();
+  keyboardOutputState = idle;
+}
+void KeyboardOutput_SchedulerAction()
+{
+  switch (keyboardOutputState)
+  {
+    case key2_state:
+      Keyboard.press(keyboardOutput_key2);
+      Serial.print("pushed Key2 @ ");
+      Serial.println(millis());
+      if (keyboardOutput_key3 == 0)
+        keyboardOutputState = releaseKeys;
+      else
+        keyboardOutputState = key3_state;
+      break;
+    case key3_state:
+      Serial.print("pushed Key3 @ ");
+      Serial.println(millis());
+      Keyboard.press(keyboardOutput_key3);
+      keyboardOutputState = releaseKeys;
+      break;
+    case releaseKeys:
+      Serial.print("Released Keys @ ");
+      Serial.println(millis());
+      Keyboard.releaseAll();
+      //if (keyboardOutput_key3 != 0)Keyboard.release(keyboardOutput_key3);
+      //if (keyboardOutput_key2 != 0)Keyboard.release(keyboardOutput_key2);
+      //if (keyboardOutput_key1 != 0)Keyboard.release(keyboardOutput_key1);
+      keyboardOutputState = idle;
+      break;
+    case idle:
+    default:
+      break;
+  }
 }
 void KeyboardOutput_Release()
 {
@@ -31,10 +75,18 @@ void KeyboardOutput_Release()
 }
 void KeyboardOutput_Press(uint8_t key1 , uint8_t key2, uint8_t key3)
 {
+  Serial.print("pushed Key1 @ ");
+  Serial.println(millis());
   Keyboard.press(key1);
-  delay(20);
-  Keyboard.press(key2);
-  delay(20);
-  Keyboard.press(key3);
-  scheduleAction(&KeyboardOutput_Release, 50);
+  keyboardOutput_key1 = key1;
+  keyboardOutput_key2 = key2;
+  keyboardOutput_key3 = key3;
+  if (key3 != 0) keyboardOutputState = key3_state;
+  if (key2 != 0) keyboardOutputState = key2_state;
+  else keyboardOutputState = releaseKeys;
+  uint8_t delayTime = 0;
+  if (key2 != 0)scheduleAction(&KeyboardOutput_SchedulerAction, 100);//key2
+  if (key3 != 0)scheduleAction(&KeyboardOutput_SchedulerAction, 200);//key3
+  scheduleAction(&KeyboardOutput_SchedulerAction, 300);//release
 }
+
